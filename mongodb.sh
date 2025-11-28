dd269798b747cd96812fa2dd27f749b9d03e4684
Chandana <rebbachandana54@gmail.com>
2025-11-28 17:53:26 +0000

    s

--- mongodb.sh
+++ mongodb.sh
@@ -29,13 +29,13 @@ fi
 cp mongo.repo /etc/yum.repos.d/
 VALIDATE $? "Coping mongo.repo"
 
-dnf install mongodb-org -y & >>$LOG_FILE
+dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "Installing Mongodb"
 
-systemctl enable mongod & >>$LOG_FILE
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enabling mongodb"
 
-systemctl start mongod & >>$LOG_FILE
+systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Start mongodb"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
